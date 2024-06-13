d70545fbda4f3d680b619a1b140dda9250118ae0
Viktória Nemkin <viktoria.nemkin@gmail.com>
2024-06-13 16:50:27 +0200

    Normalize polygons

--- src/ilp.py
+++ src/ilp.py
@@ -2,6 +2,7 @@ import numpy as np
 import cplex
 from shapely.geometry import Polygon, Point
 from shapely.affinity import translate, rotate, scale
+from shapely.ops import unary_union
 import matplotlib.pyplot as plt
 import geopandas as gpd
 
@@ -25,27 +26,22 @@ def load_all_polygons(dirpath) -> [Polygon]:
       polygons.append(polygon)
   return polygons
 
+def normalize(polygons, target=200):
+  polygons = [translate(poly, xoff=-poly.centroid.x, yoff=-poly.centroid.y) for poly in polygons]
+  union = unary_union(polygons)
+  scale_factor = target / (2*max([abs(bound) for bound in union.bounds]))
+  polygons = [scale(poly, xfact=scale_factor, yfact=scale_factor) for poly in polygons]
+  polygons = [translate(poly, xoff=-poly.centroid.x, yoff=-poly.centroid.y) for poly in polygons]
+  return polygons
 
 polygon1 = load_polygon("../data/track1/train/1/387.txt")
 polygon2 = load_polygon("../data/track1/train/1/480.txt")
+polygon3 = load_polygon("../data/track1/train/1/482.txt")
+polygons = [polygon1, polygon2, polygon3]
 
-scaled_polygon1 = scale(polygon1, xfact=1.2, yfact=1.2)
-scaled_polygon2 = scale(polygon2, xfact=1.2, yfact=1.2)
+polygons = normalize(polygons)
 
-p1 = gpd.GeoSeries(scaled_polygon1)
-p2 = gpd.GeoSeries(scaled_polygon2)
-
-fig1, ax1 = plt.subplots()
-p1.plot(ax=ax1, color='blue', edgecolor='black')
-p2.plot(ax=ax1, color='red', edgecolor='black')
-plt.show(block=False)
-
-# Create a new figure for the combined plot
-fig2, ax2 = plt.subplots()
-# Combine polygons into a GeoDataFrame
-gdf = gpd.GeoDataFrame(geometry=[scaled_polygon1, scaled_polygon2])
-
-# Plot combined GeoDataFrame
-gdf.plot(ax=ax2, cmap='tab10', edgecolor='black')
+fig, ax = plt.subplots()
+gdf = gpd.GeoDataFrame(geometry=polygons)
+gdf.plot(ax=ax, cmap='tab10', edgecolor='black')
 plt.show()
-
